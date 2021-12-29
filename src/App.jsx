1e6777ff8bb5b0a08af3b24215f4417b176fa04b
Skander Portable <skanderchebil1@gmail.com>
2021-12-29 23:50:07 +0100

    start to take form, still have to deal with the wrong city name and empty Enters

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,36 @@ function App() {
   function onEnter(e) {
     if (e.key === "Enter") {
       fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
       )
         .then((response) => response.json())
         .then((data) => setApiData(data))
         .catch((error) => console.log(error));
+      console.log(apiData.cod);
     }
   }
 
   return (
-    <div className='core'>
+    <div className="core">
       <input
-        placeholder='Enter a City Name...'
+        placeholder="Enter a City Name..."
         value={city}
         onChange={(e) => updatecity(e.target.value)}
         onKeyPress={onEnter}
       />
-
-      <h1>{city.toUpperCase()}</h1>
+      {typeof apiData === "undefined" ? (
+        <div>AAAAAAA</div>
+      ) : (
+        <div>
+          <h1>{apiData.name}</h1>
+          <h2>{Math.round(apiData.main.temp)} </h2>
+        </div>
+      )}
+      {/* {apiData.cod === "404" ? (
+        <div>{apiData.message} </div>
+      ) : (
+        <div>AAAAAAA</div>
+      )} */}
     </div>
   );
 }
