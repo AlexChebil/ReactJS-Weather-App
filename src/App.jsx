9e2d5eeb5ea22172ef1896169112d9799f97f0f8
Skander Portable <skanderchebil1@gmail.com>
2021-12-30 21:42:50 +0100

    got rid of all the bugs and added more intels to the user

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ErrorHandler from "./ErrorHandler";
 
 function App() {
   const apiKey = "c3ba5e0699ab7ddd951ea6fd02b9d372";
@@ -17,25 +18,38 @@ function App() {
   }
 
   return (
-    <div className='core'>
+    <div className="core">
       <input
-        placeholder='Enter a City Name...'
+        placeholder="Enter a City Name..."
         value={city}
         onChange={(e) => updatecity(e.target.value)}
         onKeyPress={onEnter}
       />
-      {typeof apiData === "undefined" || apiData.cod === "404" ? (
+      {typeof apiData === "undefined" ||
+      apiData.cod === "404" ||
+      apiData.cod === "400" ? (
         <h3>Enter A Valid City Name..</h3>
       ) : (
         <div>
-          <h1>{apiData.name}</h1>
-          <h2>{Math.round(apiData.main.temp)} </h2>
+          <h1>
+            {apiData.name}, {apiData.sys.country}
+          </h1>
+          <h2 className="main">
+            Temperature: {Math.round(apiData.main.temp)}{" "}
+          </h2>
+          <h2>Overall Weather: {apiData.weather[0].main} </h2>
+          <h2>Description: {apiData.weather[0].description.toUpperCase()} </h2>
+          <h2 className="main">Wind Direction : {apiData.wind.deg} Degrees </h2>
+          <h2>Wind Speed: {apiData.wind.speed}Km/h </h2>
+          <h2>
+            Geographical coordinates : <br /> Latitude: {apiData.coord.lat},
+            Longitude:
+            {apiData.coord.lon}
+          </h2>
         </div>
       )}
 
-      {apiData && apiData.cod === "404" ? (
-        <h2>{apiData.message.toUpperCase()} </h2>
-      ) : null}
+      {apiData && <ErrorHandler apiData={apiData} city={city} />}
     </div>
   );
 }
