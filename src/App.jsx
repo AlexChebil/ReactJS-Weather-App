7f0b77cca87b1e8b8f223768fbe1b730c0d1e6b0
Skander <skanderchebil1@gmail.com>
2021-12-30 18:56:38 +0100

    fixed wrong city names, still the empty Enters

--- src/App.jsx
+++ src/App.jsx
@@ -13,31 +13,29 @@ function App() {
         .then((response) => response.json())
         .then((data) => setApiData(data))
         .catch((error) => console.log(error));
-      console.log(apiData.cod);
     }
   }
 
   return (
-    <div className="core">
+    <div className='core'>
       <input
-        placeholder="Enter a City Name..."
+        placeholder='Enter a City Name...'
         value={city}
         onChange={(e) => updatecity(e.target.value)}
         onKeyPress={onEnter}
       />
-      {typeof apiData === "undefined" ? (
-        <div>AAAAAAA</div>
+      {typeof apiData === "undefined" || apiData.cod === "404" ? (
+        <h3>Enter A Valid City Name..</h3>
       ) : (
         <div>
           <h1>{apiData.name}</h1>
           <h2>{Math.round(apiData.main.temp)} </h2>
         </div>
       )}
-      {/* {apiData.cod === "404" ? (
-        <div>{apiData.message} </div>
-      ) : (
-        <div>AAAAAAA</div>
-      )} */}
+
+      {apiData && apiData.cod === "404" ? (
+        <h2>{apiData.message.toUpperCase()} </h2>
+      ) : null}
     </div>
   );
 }
