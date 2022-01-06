edde9c2330d10508acdf61a70a63b8a0595be57d
Skander <skanderchebil1@gmail.com>
2022-01-06 01:55:08 +0100

    added live seconds and optimized code for the DateHandler component

--- src/Components/DateHandler.jsx
+++ src/Components/DateHandler.jsx
@@ -1,42 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function DateHandler() {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
   const date = new Date();
-  const day = days[date.getDay()];
-  const dayNum = date.getDate();
-  const month = months[date.getMonth()];
-  const year = date.getFullYear();
+  const [dummyState, setDummyState] = useState();
+  let seconds = date.getSeconds();
+  let fullDate = date.toDateString();
+
+  useEffect(() => {
+    const inter = setInterval(() => {
+      setDummyState(true);
+    }, 1000);
+    return () => {
+      clearInterval(inter);
+      setDummyState(false);
+    };
+  }, [dummyState]); // this useEffect AND setInterval refresh the entire component every 1second to get new values
+
+  let time = date.getHours() + " : " + date.getMinutes() + " : " + seconds;
 
   return (
     <div>
-      <span>{day} </span>
-      <span>{dayNum} </span>
-      <span>{month} </span>
-      <span>{year} </span>
+      <div>{fullDate} </div>
+      <div>{time} </div>
     </div>
   );
 }
